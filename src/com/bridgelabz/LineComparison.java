1570612a77fdd46ccc5cdaed091354a75a98b8f7
Akshada <akshumore96@gmail.com>
2022-03-26 15:09:07 +0530

    Updated using OOPS concept

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -4,28 +4,38 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class LineComparison {
-    public static void main(String args[]) {
-        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+    static int x1, x2, y1, y2;
+    void getCoordinates() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter co-ordinates for line1 (x1, y1) : ");
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        System.out.println("Enter co-ordinates for line1 (x2, y2) : ");
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
-        double length1 = Math.sqrt((x2-x1)^2+(y2-y1)^2);
-        System.out.println("Enter co-ordinates for line2 (x3, y3) : ");
-        int x3 = sc.nextInt();
-        int y3 = sc.nextInt();
-        System.out.println("Enter co-ordinates for line2 (x4, y4) : ");
-        int x4 = sc.nextInt();
-        int y4 = sc.nextInt();
-        double length2 = Math.sqrt((x4-x3)^2+(y4-y3)^2);
+        System.out.println("Enter (x1, y1) : ");
+        x1 = sc.nextInt();
+        y1 = sc.nextInt();
+        System.out.println("Enter (x2, y2) : ");
+        x2 = sc.nextInt();
+        y2 = sc.nextInt();
+    }
+    double lineLength() {
+        return Math.sqrt((x2-x1)^2+(y2-y1)^2);
+    }
+    void lineCompare(double length1, double length2) {
         if (length1 == length2)
             System.out.println("Line1 is equal to Line2");
         else if (length1 > length2)
             System.out.println("Line1 is greater than Line2");
         else
             System.out.println("Line1 is less than Line2");
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+        LineComparison lc1 = new LineComparison();
+        System.out.println("Enter line1 co-ordinates");
+        lc1.getCoordinates();
+        double length1 = lc1.lineLength();
+        LineComparison lc2 = new LineComparison();
+        System.out.println("Enter line2 co-ordinates");
+        lc2.getCoordinates();
+        double length2 = lc2.lineLength();
+        System.out.println("Comparing line1 & line2");
+        lc1.lineCompare(length1, length2);
     }
 }
